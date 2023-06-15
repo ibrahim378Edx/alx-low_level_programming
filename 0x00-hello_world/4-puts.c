36df65256cb918914393c3e4cf47495dbdd23272
ibrahim378Edx <werdewer@yahoo.com>
2023-06-15 23:38:40 +0300

    task 4 styling

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,11 @@
-#include <stdio.h>
+#include<stdio.h>
 /**
  * main - Entry point
- * 
- * Description: 'the program's description'
- * @parameter: describe the parameter
- * 
+ *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-char stg[100] = "\"Programming is like building a multilingual puzzle";
-puts(stg);
-return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
