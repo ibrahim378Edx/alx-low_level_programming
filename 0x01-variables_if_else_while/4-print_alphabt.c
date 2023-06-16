8f3d0aa40395cd242da40b3b84b91dea0e8fc2d8
ibrahim378Edx <werdewer@yahoo.com>
2023-06-16 23:24:03 +0300

    task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char z;
+
+int i;
+
+z = 'a';
+
+for (i = 0; i <= 25; i++)
+{
+	if (z != 101 && z != 113)
+	{
+		putchar(z);
+	}
+z += 1;
+}
+putchar('\n');
+return (0);
+}
+
