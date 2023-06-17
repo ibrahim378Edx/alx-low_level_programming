39834535689a6bc2db03a8dcb5e3616d3c0d809b
ibrahim378Edx <werdewer@yahoo.com>
2023-06-17 03:50:06 +0300

    task 11 betty fix

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -6,31 +6,28 @@
  */
 int main(void)
 {
-        int z;
-        int x;
-	int y;
+int z;
+int x;
+int y;
 for (z = 0; z <= 9; z++)
 {
-        for (x = 0; x <= 9; x++)
-        {
+	for (x = 0; x <= 9; x++)
+	{
 		for (y = 0; y <= 9; y++)
 		{
-
-               		if (z < x && x < y )
-                	{
-                        	putchar(z + '0');
-                       		putchar(x + '0');
+			if (z < x && x < y)
+			{
+				putchar(z + '0');
+				putchar(x + '0');
 				putchar(y + '0');
-                	}
-
-
-                	if (z != 7 && x < y  && z != x && z < x)
-                	{
-                        	putchar(',');
-                        	putchar(' ');
-                	}
+			}
+			if (z != 7 && x < y  && z != x && z < x)
+			{
+				putchar(',');
+				putchar(' ');
+			}
 		}
-        }
+	}
 }
 putchar('\n');
 return (0);
