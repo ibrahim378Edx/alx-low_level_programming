bdf7183ce407cfe2abccf8425004b5fa84bd74d3
ibrahim378Edx <werdewer@yahoo.com>
2023-06-21 00:52:49 +0300

    task 4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _islower - checks for a charater if it is lower or uppercase
+ * @a: parameter which takes charaters and turns them to ascii value
+ *
+ * Return: 1 if lowercase returns 0 if upper
+ */
+int _isalpha(int a)
+{
+if ((a >= 97 || a <= 122 ) && (a >= 65 || a <= 90))
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
