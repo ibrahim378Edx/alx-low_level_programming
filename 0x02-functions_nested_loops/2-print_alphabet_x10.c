249afa06edfed04745782639066ff39f3a68c02b
ibrahim378Edx <werdewer@yahoo.com>
2023-06-21 00:05:12 +0300

    task2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - function that repeats print alpha 10 times
+ *
+ * Return: 0 for success
+ */
+int main(void)
+{
+int x;
+x = 0;
+while (x < 10)
+{
+print_alphabet();
+x++;
+}
+return (0);
+}
