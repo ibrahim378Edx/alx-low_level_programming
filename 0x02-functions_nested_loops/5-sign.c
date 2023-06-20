64d551f7773027875604d74d3baaf2e9a7b61385
ibrahim378Edx <werdewer@yahoo.com>
2023-06-21 01:46:22 +0300

    task 5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_sign - checks for ascii values
+ *
+ * @n: int value turns into ascii
+ *
+ * Return: 0 if equal to 0 , 1 if bigger than 0 , else -1
+ */
+int print_sign(int n)
+{
+if (n == 0)
+{
+_putchar('0');
+return (0);
+}
+else if (n < 0)
+{
+_putchar('1')
+return (1);
+}
+else
+{
+_putchar('-1');
+return (-1);
+}
+}
