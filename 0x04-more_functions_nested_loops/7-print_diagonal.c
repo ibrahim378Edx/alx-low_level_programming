f05c887512965ed2901282096658afb44d2176ec
ibrahim378Edx <werdewer@yahoo.com>
2023-06-22 23:15:57 +0300

    task 7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_diagonal - prints space and a back slash
+ *
+ * @n: number of times spaces and new lines prints
+ *
+ * Return: nothing
+ */
+void print_diagonal(int n)
+{
+int i, l;
+for (i = 0; i < n; i++)
+{
+	for (l = n - i; l < n; l++)
+	{
+		_putchar(' ');
+	}
+_putchar(92);
+_putchar('\n');
+}
+}
+
