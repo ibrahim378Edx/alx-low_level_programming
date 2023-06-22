6121f83fc7513cfa07967edfa9cd7f200941df6a
ibrahim378Edx <werdewer@yahoo.com>
2023-06-23 02:02:44 +0300

    task 10

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * print_triangle - this function makes triangle using # symbol
+ *
+ * @size: triangle height
+ *
+ * Return: nothing
+ */
+void print_triangle(int size)
+{
+int i, l, q;
+for (i = 0; i < size; i++)
+{
+        for (l = i; l < size; l++)
+        {
+                _putchar(' ');
+        }
+	for (q = size - i; q < size; q++)
+        {
+                _putchar('#');
+        }
+
+_putchar('\n');
+}
+if (size <= 0)
+{
+_putchar('\n');
+}
+}
