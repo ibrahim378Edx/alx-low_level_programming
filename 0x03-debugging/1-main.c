2deaa546aa2154ceb9fd09816a0041914d481487
ibrahim378Edx <werdewer@yahoo.com>
2023-06-23 06:02:31 +0300

    task 1

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -14,7 +14,6 @@ printf("Infinite loop incoming :(\n");
 i = 0;
 
 /*
- *
  * while (i < 10)
  * {
  * putchar(i);
@@ -22,6 +21,7 @@ i = 0;
  */
 
 printf("Infinite loop avoided! \\o/\n");
+
 
 return (0);
 }
