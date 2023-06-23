18e577fee5c046fd05ea0e982157ad70312aa0ba
ibrahim378Edx <werdewer@yahoo.com>
2023-06-23 05:31:27 +0300

    task 11

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -2,22 +2,20 @@
 #include <math.h>
 
 int main(void) {
-    long num = 612852475143;
-    long max_factor = 1;
-    long factor = 2;
-
-    while (num > 1) {
-        if (num % factor == 0) {
-            max_factor = factor;
-            num /= factor;
-            while (num % factor == 0) {
-                num /= factor;
-            }
-        }
-        factor = (factor == 2) ? 3 : factor + 2;
-    }
-
-    printf("%ld\n", max_factor);
-    return 0;
+long num = 612852475143;
+long max_factor = 1;
+long factor = 2;
+while (num > 1) {
+if (num % factor == 0) {
+max_factor = factor;
+num /= factor;
+while (num % factor == 0) {
+num /= factor;
+}
+}
+factor = (factor == 2) ? 3 : factor + 2;
+}
+printf("%ld\n", max_factor);
+return 0;
 }
 
