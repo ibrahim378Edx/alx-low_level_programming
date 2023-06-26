aa6c3f655737722b72893986e48c9ca270644400
ibrahim378Edx <werdewer@yahoo.com>
2023-06-26 21:37:56 +0300

    task 1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * swap_int - swap values
+ *
+ * @n: holds the value that im gonna changes
+ *
+ * Return: nothing i dont  need to using pointers mostly atleast for this
+ */
+void swap_int(int *a, int *b)
+{
+int tmp;
+tmp = *a
+a* = *b
+*b = tmp
+}
