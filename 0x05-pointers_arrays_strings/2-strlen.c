12a7c8d94ec94bdb90f8ed32fad4bf2b85eaa9b8
ibrahim378Edx <werdewer@yahoo.com>
2023-06-26 22:14:51 +0300

    task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strlen - gets the string length without null charater
+ *
+ * @s: string to count its length
+ *
+ * Return: value of string length
+ */
+int _strlen(char *s)
+{
+int i;
+i = 0;
+while (s[i] != NULL)
+i++;
+}
+return (i);
