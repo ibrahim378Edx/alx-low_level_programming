fa286824eaf592373ef4550d21283da962e9b7cd
ibrahim378Edx <werdewer@yahoo.com>
2023-06-27 02:58:56 +0300

    task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+/**
+ * print_rev - prints string in reverse
+ *
+ * @s: string to reverse
+ *
+ * Return: value of string length
+ */
+void print_rev(char *s)
+{
+char c = '\n';
+int clen = strlen(s);
+char *n = malloc(clen + 1);
+int i, j;
+for (i = clen - 1, j = 0; i >= 0; i--, j++)
+{
+n[j] = s[i];
+}
+write(STDOUT_FILENO, n, strlen(n));
+write(STDOUT_FILENO, str, strlen(str));
+free(n);
+}
