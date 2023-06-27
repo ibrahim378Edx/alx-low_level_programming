6155b90cf285e96630168576e4e88bc107f281a3
ibrahim378Edx <werdewer@yahoo.com>
2023-06-27 03:36:56 +0300

    task 2

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -20,9 +20,7 @@ for (i = clen - 1, j = 0; i >= 0; i--, j++)
 {
 n[j] = s[i];
 }
-write(STDOUT_FILENO, s, strlen(s));
-write(STDOUT_FILENO, &c, sizeof(c));
-write(STDOUT_FILENO, n, strlen(n));
+s = n
 write(STDOUT_FILENO, &c, sizeof(c));
 free(n);
 }
