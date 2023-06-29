bd82855733d18afa703d46d237792bcadf206d86
ibrahim378Edx <werdewer@yahoo.com>
2023-06-29 22:51:53 +0300

    task 3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * _strcmp - function that compares 2 strings
+ *
+ * @s1: 1st string
+ *
+ * @src: the source string that will copy
+ *
+ * Return: 0 for same 1 if different
+ */
+int _strcmp(char *s1, char *s2)
+{
+int len1, i;
+len1 = strlen(s1);
+i = 0;
+while(i < len1)
+{
+if(*(s1 + i) == *(s2 + i))
+{
+i++;
+}
+else if(*(s1 + i) < *(s2 + i))
+{
+return (-15);
+}
+else
+{
+return (15);
+}
+}
+return (0);
+}
