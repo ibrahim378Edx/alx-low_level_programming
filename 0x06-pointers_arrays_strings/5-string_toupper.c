75579d3a773c1f699c78f94693f014610fe42ef1
ibrahim378Edx <werdewer@yahoo.com>
2023-06-30 02:56:34 +0300

    task 5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+/**
+ * string_toupper - function makes all letters upper
+ *
+ * @s: string to be converted
+ *
+ * Return: the string after conversion
+ */
+char *string_toupper(char *s)
+{
+int i, len;
+len = strlen(s);
+i = 0;
+while (i < len - 1)
+{
+if ((*(s + i) <= 90 || *(s + i) > 65) && (*(s + i) <= 122 || *(s + i) >= 97))
+{
+if (*(s + i) >= 97 && *(s + i) <= 122)
+{
+*(s + i) = *(s + i) -32;
+}
+}
+i++;
+}
+return (s);
+}
