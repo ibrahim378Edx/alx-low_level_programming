a20b8f853014f4eec052c8ecefd97a4e8980601f
ibrahim378Edx <werdewer@yahoo.com>
2023-07-04 01:49:15 +0300

    task 3 betty

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -19,15 +19,15 @@ unsigned int counter;
 counter = 1;
 len = strlen(s);
 len1 = strlen(accept);
-for(i = 0; i <= len; i++)
+for (i = 0; i <= len; i++)
 {
-	for(z = 0; z <= len1; z++)
+	for (z = 0; z <= len1; z++)
 	{
 		if (*(s + z) == *(accept + i))
-				{
-				counter++;
-				break;
-				}
+		{
+			counter++;
+			break;
+		}
 	}
 }
 return (counter);
