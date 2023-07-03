865640223225f6a7f67c864c15edbe1d5faac574
ibrahim378Edx <werdewer@yahoo.com>
2023-07-03 21:12:19 +0300

    task 0 test;

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+/**
+ * _memset - fills bytes with value
+ *
+ * @s: array to be changed
+ *
+ * @b: value to change to
+ *
+ * @n: number of bytes to change
+ *
+ * Return: pointer to the array
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+int i;
+i = 0;
+while (i < n)
+{
+*(s + i) = b
+}
+return (s);
+}
