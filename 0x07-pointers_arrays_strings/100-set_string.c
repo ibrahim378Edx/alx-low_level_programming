072ba7ac26684601d444f3912084fc7dcd084d77
ibrahim378Edx <werdewer@yahoo.com>
2023-07-04 05:21:17 +0300

    task 8

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * set_string - sets a string from one to another
+ *
+ * @s: to copy to
+ *
+ * @to: to copy from
+ *
+ * Return: nothing
+ */
+void set_string(char **s, char *to)
+{
+*s = to;
+}
