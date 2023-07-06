ea56e34c3ef0f7c2629d40b355376cd9406a1622
ibrahim378Edx <werdewer@yahoo.com>
2023-07-06 22:29:59 +0300

    task 6

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -11,7 +11,7 @@ int is_prime_helper(int n, int i);
  */
 int is_prime_number(int n)
 {
-return is_prime_helper(n, 2); 
+return (is_prime_helper(n, 1));
 }
 /**
  * is_prime_helper - another function because we can use basic shit
@@ -22,18 +22,19 @@ return is_prime_helper(n, 2);
  *
  * Return: the actual number if prime or not
  */
-int is_prime_helper(int n, int i) {
-if (n < 2) 
+int is_prime_helper(int n, int i)
 {
-return 0;
-}
-if (i > sqrt(n))
+if (n <= 1)
 {
-return 1;
+return (0);
 }
-if (n % i == 0)
+if ((n / i) < i)
 {
-return 0;
+return (1);
 }
-return is_prime_helper(n, i + 1);
+if (n % i == 0 && i > 1)
+{
+return (0);
+}
+return (is_prime_helper(n, i + 1));
 }
