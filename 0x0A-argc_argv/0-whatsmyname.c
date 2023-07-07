97ffb187cd2faddc5455eef2d1063dccfea19334
ibrahim378Edx <werdewer@yahoo.com>
2023-07-08 01:44:52 +0300

    task 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -8,9 +8,9 @@
  *
  * Return: nothing
  */
-int main (int argc, char *argv[])
+int main(int argc, char *argv[])
 {
-(void) argc;
-printf("%s\n",argv[0]);
+(void)argc;
+printf("%s\n", argv[0]);
 return (0);
 }
