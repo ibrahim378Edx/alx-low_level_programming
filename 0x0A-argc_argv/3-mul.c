63d0bc78deb16da8aa1a035848c648ae8c52d27e
ibrahim378Edx <werdewer@yahoo.com>
2023-07-08 02:11:10 +0300

    task 3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - multiplies argv
+ *
+ * @argc: well argc
+ *
+ * @argv: well argv
+ *
+ * Return: nothing
+ */
+int main(int argc, char *argv[])
+{
+if (argc == 3)
+{
+printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+return (0);
+}
+else
+{
+printf("Error\n");
+return (1);
+}
+}
