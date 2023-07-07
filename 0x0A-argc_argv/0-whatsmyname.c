8f9901543bec380a1742aaf6c8c106b684443bab
ibrahim378Edx <werdewer@yahoo.com>
2023-07-08 01:39:43 +0300

    task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - well holds the main code
+ *
+ * @argv: well argv
+ *
+ * @argc: u know argc
+ *
+ * Return: nothing
+ */
+int main (int argc, char *argv[])
+{
+printf("%s\n",argv[argc - 1]);
+return (0);
+}
