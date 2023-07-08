8de52a6b44e7c0b01a71892d07aa5088312fd55a
ibrahim378Edx <werdewer@yahoo.com>
2023-07-08 04:34:49 +0300

    task 4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
+#include <string.h>
 /**
  * main - adds numbers
  *
@@ -13,13 +14,16 @@
 int main(int argc, char *argv[])
 {
 int i, z;
-
+unsigned long int l;
 z = 0;
+
 if (argc > 1)
 {
 for (i = 1; i < argc; i++)
 {
-if (*argv[i] < 48 || *argv[i] > 57)
+for (l = 0; l < strlen(argv[i]); l++)
+{
+if (argv[i][l] < 48 || argv[i][l] > 57)
 {
 printf("Error\n");
 return (1);
@@ -30,10 +34,11 @@ z += atoi(argv[i]);
 }
 }
 }
+printf("%d\n", z);
+}
 else
 {
 printf("0\n");
 }
-printf("%d\n", z);
 return (0);
 }
