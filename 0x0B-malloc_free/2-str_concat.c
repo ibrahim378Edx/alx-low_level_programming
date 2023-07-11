1c1c8387ef03cfb54c496095d7b5c1d6ec5cba6d
ibrahim378Edx <werdewer@yahoo.com>
2023-07-11 18:15:55 +0300

    task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,6 +1,10 @@
 #include "main.h"
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
+int length(char *w);
+
 /**
  * str_concat - concatenate 2 strings together
  *
@@ -14,34 +18,68 @@ char *str_concat(char *s1, char *s2)
 {
 int z, l1, l2, i;
 char *q;
-
-i = 0;
-l1 = strlen(s1);
-l2 = strlen(s2);
-q = malloc(sizeof(char) * (l1 + l2 + 2));
-z = 0;
-if (q == NULL)
+i = 0, l1 = length(s1), l2 = length(s2), z = 0;
+q = malloc(sizeof(char) * (l1 + l2 + 1));
+if (s1 == NULL && s2 == NULL)
 {
-return (NULL);
+q[0] = '\0';
+return (q);
 }
-while (s1[z] !=  '\0' && s1 != NULL)
+else if (s2 == NULL && s1 != NULL)
 {
-if (s1 == NULL)
+while (s1[z] !=  '\0')
 {
-break;
-}
 q[z] = s1[z];
 z++;
 }
-while (s2[i] != '\0' && s2 != NULL)
-{
-if (s2 == NULL)
-{
-break;
+return (q);
 }
+else if (s1 == NULL && s2 != NULL)
+{
+while (s2[i] != '\0')
+{
 q[z] = s2[i];
 i++;
 z++;
 }
 return (q);
+}
+else if (s1 != NULL && s2 != NULL)
+{
+while (s1[z] != '\0')
+{
+q[z] = s1[z];
+z++;
+}
+while (s2[i] != '\0')
+{
+q[z] = s2[i];
+z++;
+i++;
+}
+return (q);
+}
+return (q);
+}
+
+/**
+ * length - gets length of string
+ *
+ *  @w: string
+ *
+ * Return: length
+*/
+
+int length(char *w)
+{
+int l1;
+l1 = 0;
+if (w != NULL)
+{
+while (w[l1] != '\0')
+{
+l1++;
+}
+}
+return (l1);
 }
