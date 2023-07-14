a385a92389556e235b034285ee6f9b2c71256ed0
ibrahim378Edx <werdewer@yahoo.com>
2023-07-14 22:33:46 +0300

    task 1

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+/**
+ * array_range - puts values of array into another
+ *
+ * @min: minmum value
+ *
+ * @max: max value
+ *
+ * Return: pointer to the new array
+ */
+int *array_range(int min, int max)
+{
+int *q, i, l;
+i = 0, l = 0;
+if (min > max)
+{
+return (NULL);
+}
+while (i <= max)
+{
+i++;
+}
+q = malloc(sizeof(int) * i);
+if (q == NULL)
+{
+return (NULL);
+}
+while (l <= max)
+{
+q[l] = l + min;
+l++;
+}
+return (q);
+}
