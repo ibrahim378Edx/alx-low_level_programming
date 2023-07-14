4053ba5fb62081288b805560da19cc0323e23efd
ibrahim378Edx <werdewer@yahoo.com>
2023-07-14 13:38:43 +0300

    task 1

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,77 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+int lengther(char *s);
+/**
+ * string_nconcat - a function to concatenate 2 strings
+ *
+ * @s1: string 1
+ *
+ * @s2: string 2
+ *
+ * @n: amount of charaters to take from s2
+ *
+ * Return: pointer to the new string
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+char *new;
+unsigned int l1, i, j;
+l1 = lengther(s1), i = 0, j = 0;
+new = malloc(sizeof(char) * (n + l1));
+if (new == NULL)
+{
+return (NULL);
+}
+if (s1 == NULL)
+{
+while (s2[i] != '\0' && i < n)
+{
+new[i] = s2[i];
+i++;
+}
+return (new);
+}
+if (s2 == NULL)
+{
+while (s1[i] != '\0')
+{
+new[i] = s1[i];
+i++;
+}
+return (new);
+}
+if (s1 != NULL && s2 != NULL)
+{
+while (s1[i] != '\0')
+{
+new[i] = s1[i];
+i++;
+}
+while (s2[j] != '\0' && j < n)
+{
+new[i] = s2[j];
+i++;
+j++;
+}
+}
+new[i + 1] = '\0';
+return (new);
+}
+/**
+ * lengther - gets length of string
+ *
+ * @s: string
+ *
+ * Return: the lengther
+ */
+int lengther(char *s)
+{
+int l1;
+l1 = 0;
+while (s[l1] != '\0')
+{
+l1++;
+}
+return (l1);
+}
