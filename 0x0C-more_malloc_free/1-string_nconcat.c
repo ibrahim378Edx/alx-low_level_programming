c31d049752e053b103528ba44308f377d92ee0c9
ibrahim378Edx <werdewer@yahoo.com>
2023-07-14 14:16:25 +0300

    task 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -16,16 +16,23 @@ int lengther(char *s);
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 char *new;
-unsigned int l1, i, j;
-l1 = lengther(s1), i = 0, j = 0;
-new = malloc(sizeof(char) * (n + l1));
+unsigned int l1, l2, i, j;
+l1 = lengther(s1), l2 = lengther(s2), i = 0, j = 0;
+if (n < l2)
+{
+new = malloc(sizeof(char) * (n + l1 + 1));
+}
+else if (n > l2)
+{
+new = malloc(sizeof(char) * (l2 + l1 + 1));
+}
 if (new == NULL)
 {
 return (NULL);
 }
 if (s1 == NULL)
 {
-while (s2[i] != '\0' || i < n)
+while (s2[i] != '\0' && i < n)
 {
 new[i] = s2[i];
 i++;
