a3c8964ebf566f5072f54f462ece8eb909253dbd
ibrahim378Edx <werdewer@yahoo.com>
2023-07-18 01:38:26 +0300

    task 0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,15 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - saves dogs info
+ * @name: dog name
+ * @age: dog age
+ * @owner: ownder name
+ *
+ * Description: a struct that saves dog's info
+ */
+
 struct dog
 {
 char *name;
