33002cab6a716f1f69cbdbe82dc850493dd64807
ibrahim378Edx <werdewer@yahoo.com>
2023-07-18 00:14:50 +0300

    task 2 betty

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 
 #define NAME __FILE__
-
+/**
+ * main - prints file name
+ *
+ * Return: 0 always
+ */
 int main(void)
 {
-printf("%s\n",NAME);
+printf("%s\n", NAME);
 return (0);
 }
