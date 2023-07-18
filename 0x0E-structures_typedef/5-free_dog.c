705d8f7d8cba5a271cbea386d1c4a820612fab31
ibrahim378Edx <werdewer@yahoo.com>
2023-07-18 22:14:32 +0300

    task 5

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * free_dog - freeing struct of dog
+ *
+ * @d: variable to free
+ *
+ * Return: nothing
+ */
+void free_dog(dog_t *d)
+{
+free(d->owner);
+free(d->name);
+free(d);
+}
