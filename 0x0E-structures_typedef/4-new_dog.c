13a0d940d3f9ccf3c22751dd7b3599eb6e9cac86
ibrahim378Edx <werdewer@yahoo.com>
2023-07-18 21:27:32 +0300

    task 4

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include "dog.h"
+#include <string.h>
+#include <stdlib.h>
+/**
+ * new_dog - making new variable
+ *
+ * @name: dog's name
+ *
+ * @age: dog's age
+ *
+ * @owner: dog's owner
+ *
+ * Return: a struct of new varible
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+dog_t *q = malloc(sizeof(dog_t));
+if (q == NULL)
+{
+return (NULL);
+}
+q->owner = malloc(sizeof(char) * (strlen(owner) + 1));
+q->name = malloc(sizeof(char) * (strlen(name) + 1));
+if (q->owner == NULL || q->name == NULL)
+{
+return (NULL);
+}
+q->age = age;
+//q->name = name;
+//q->owner = owner;
+strcpy(q->name, name);
+strcpy(q->owner, owner);
+return (q);
+}
