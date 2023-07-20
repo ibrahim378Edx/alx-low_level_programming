1ba8797934d1609b945e298ce3a36bb962123469
ibrahim378Edx <werdewer@yahoo.com>
2023-07-21 02:09:20 +0300

    task 3

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,93 +1,60 @@
 #include <stdarg.h>
 #include <stdio.h>
 
+typedef struct {
+    char c;
+    int i;
+    float f;
+    char* s;
+} argument;
+
 void print_all(const char* const format, ...)
 {
     const char* p = format;
     va_list args;
-    int i;
-    char c;
-    double f;
-    char* s;
+    argument arg;
+    int printed = 0;
 
     va_start(args, format);
 
-    while (*p)
+    while (*p != '\0')
     {
-        if (*(p + 1) == '\0')
-        {
-            switch (*p)
-            {
-                case 'c':
-                    c = (char) va_arg(args, int);
-                    printf("%c", c);
-                    break;
-
-                case 'i':
-                    i = va_arg(args, int);
-                    printf("%d", i);
-                    break;
-
-                case 'f':
-                    f = va_arg(args, double);
-                    printf("%f", f);
-                    break;
-
-                case 's':
-                    s = va_arg(args, char*);
-                    if (s == NULL)
-                    {
-                        printf("(nil)");
-                    }
-                    else
-                    {
-                        printf("%s", s);
-                    }
-                    break;
-
-                default:
-                    break;
-            }
-        }
-        else
-        {
-            switch (*p)
-            {
-                case 'c':
-                    c = (char) va_arg(args, int);
-                    printf("%c, ", c);
-                    break;
-
-                case 'i':
-                    i = va_arg(args, int);
-                    printf("%d, ", i);
-                    break;
-
-                case 'f':
-                    f = va_arg(args, double);
-                    printf("%f, ", f);
-                    break;
-
-                case 's':
-                    s = va_arg(args, char*);
-                    if (s == NULL)
-                    {
-                        printf("(nil), ");
-                    }
-                    else
-                    {
-                        printf("%s, ", s);
-                    }
-                    break;
-
-                default:
-                    break;
-            }
+        switch (*p) {
+            case 'c':
+                arg.c = va_arg(args, int);
+                printf("%s%c", printed ? ", " : "", arg.c);
+                printed = 1;
+                break;
+            case 'i':
+                arg.i = va_arg(args, int);
+                printf("%s%d", printed ? ", " : "", arg.i);
+                printed = 1;
+                break;
+            case 'f':
+                arg.f = va_arg(args, double);
+                printf("%s%f", printed ? ", " : "", arg.f);
+                printed = 1;
+                break;
+            case 's':
+                arg.s = va_arg(args, char*);
+                if (arg.s == NULL)
+                {
+                    printf("%s(nil)", printed ? ", " : "");
+                }
+                else
+                {
+                    printf("%s%s", printed ? ", " : "", arg.s);
+                }
+                printed = 1;
+                break;
+            default:
+                break;
         }
 
         p++;
     }
 
     va_end(args);
+
     printf("\n");
 }
