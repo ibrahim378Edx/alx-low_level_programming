64e896803b02aa3ca1e12fde454e35af394ed1f5
ibrahim378Edx <werdewer@yahoo.com>
2023-07-28 02:22:56 +0300

    betty fix

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -2,9 +2,16 @@
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
+/**
+ * free_list - frees linked lists
+ *
+ * @head: head of the list
+ *
+ * Return: nothing
+*/
 void free_list(list_t *head)
 {
-if(head == NULL)
+if (head == NULL)
 {
 return;
 }
