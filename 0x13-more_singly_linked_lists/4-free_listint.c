876666757e21202ca37cf146263418ff1e4f4212
ibrahim378Edx <werdewer@yahoo.com>
2023-08-01 11:40:24 +0300

    task 4

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+/**
+ * free_listint - frees a list
+ *
+ * @head: pointer to the head of the list
+ *
+ * Return: nothing
+*/
+void free_listint(listint_t *head)
+{
+if(head != NULL)
+{
+free_listint(head->next);
+}
+free(head);
+}
