c3b8b6db3a70baed3172e7e0d703f636b531492b
ibrahim378Edx <werdewer@yahoo.com>
2023-08-02 04:04:33 +0300

    task 10 betty

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+/**
+ * reverse_listint - reverses linked list
+ *
+ * @head: pointer to the head of the linked list
+ *
+ * Return: pointer to head of the linked list after reverse
+*/
+listint_t *reverse_listint(listint_t **head)
+{
+listint_t *onest = NULL;
+listint_t *second = NULL;
+if (head != NULL)
+{
+while (*head)
+{
+second = *head;
+*head = (*head)->next;
+second->next = onest;
+onest = second;
+}
+*head = onest;
+return (*head)
+}
+return(NULL);
+}
