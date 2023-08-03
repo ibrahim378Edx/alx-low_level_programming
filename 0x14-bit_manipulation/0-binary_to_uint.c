681a3bf5390b82923c434643dc45a088bbb09d9a
ibrahim378Edx <werdewer@yahoo.com>
2023-08-04 01:25:53 +0300

    task0 betty

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -12,25 +12,24 @@ unsigned int binary_to_uint(const char *b)
 {
 	unsigned int l = 0, q = 0, s = 0;
 
-	if (b == NULL)
-    {
-		return (0);
-    }
-	l = lengther(b);
-	while (l--)
-	{
-		if (b[l] != 48 && b[l] != 49)
-        {
-			return (0);
-        }
-		if (b[l] == 49)
-        {
-			s += 1 << q;
-        }
-		q++;
-	}
-
-	return (s);
+if (b == NULL)
+{
+return (0);
+}
+l = lengther(b);
+while (l--)
+{
+if (b[l] != 48 && b[l] != 49)
+{
+return (0);
+}
+if (b[l] == 49)
+{
+s += 1 << q;
+}
+q++;
+}
+return (s);
 }
 
 /**
@@ -42,11 +41,10 @@ unsigned int binary_to_uint(const char *b)
   */
 int lengther(const char *str)
 {
-	int count = 0;
-
-	while (str[count])
-    {
-		count++;
-    }
-	return (count);
+int count = 0;
+while (str[count])
+{
+count++;
+}
+return (count);
 }
