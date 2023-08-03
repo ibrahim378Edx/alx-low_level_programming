ace82729fc4a142698109efb1ca7fe4425ed0c84
ibrahim378Edx <werdewer@yahoo.com>
2023-08-04 02:45:57 +0300

    task 1

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,44 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+  * print_binary - calculate and prints the binary value
+  *
+  * @n: The value to calculate
+  *
+  * Return: Nothing
+  */
+void print_binary(unsigned long int n)
+{
+if (n == 0)
+{
+_putchar('0');
+return;
+}
+calcBinary(n);
+}
+
+/**
+  * calcBinary - calc the binary value
+  *
+  * @n: the value to calc
+  *
+  * Return: nothing it just prints
+  */
+void calcBinary(unsigned long int n)
+{
+if (n < 1)
+{
+return;
+}
+calcBinary(n >> 1);
+if (n & 1)
+{
+_putchar('1');
+}
+else
+{
+_putchar('0');
+}
+}
