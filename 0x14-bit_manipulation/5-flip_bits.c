9b7274b596f31f41f97fe737ea94d6b3114c3091
ibrahim378Edx <werdewer@yahoo.com>
2023-08-04 05:29:33 +0300

    task 5

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+  * flip_bits - finds out the number of flips to switch a number to another
+  *
+  * @n: first number 
+  *
+  * @m: second number 
+  *
+  * Return: number of flips needed
+  */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+unsigned long int z = 0, counter = 0;
+z = n ^ m;
+while (z)
+{
+if (z & 1)
+{
+counter++;
+}
+z = z >> 1;
+}
+return (counter);
+}
